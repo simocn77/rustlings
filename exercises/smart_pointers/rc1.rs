259371376f774fe25b3dead196b7518907ff7d85
Simone Cianni <simone.cianni@adesso.ch>
2023-11-21 10:55:00 +0100

    v 5.6.1: solutions
    Signed-off-by: Simone Cianni <simone.cianni@adesso.ch>

--- exercises/smart_pointers/rc1.rs
+++ exercises/smart_pointers/rc1.rs
@@ -10,8 +10,6 @@
 //
 // Execute `rustlings hint rc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::rc::Rc;
 
 #[derive(Debug)]
@@ -38,7 +36,7 @@ impl Planet {
 #[test]
 fn main() {
     let sun = Rc::new(Sun {});
-    println!("reference count = {}", Rc::strong_count(&sun)); // 1 reference
+    println!("({}) : reference count = {}", line!(), Rc::strong_count(&sun)); // 1 reference
 
     let mercury = Planet::Mercury(Rc::clone(&sun));
     println!("reference count = {}", Rc::strong_count(&sun)); // 2 references
@@ -61,18 +59,18 @@ fn main() {
     jupiter.details();
 
     // TODO
-    let saturn = Planet::Saturn(Rc::new(Sun {}));
+    let saturn = Planet::Saturn(Rc::clone(&sun));
     println!("reference count = {}", Rc::strong_count(&sun)); // 7 references
     saturn.details();
 
     // TODO
-    let uranus = Planet::Uranus(Rc::new(Sun {}));
+    let uranus = Planet::Uranus(Rc::clone(&sun));
     println!("reference count = {}", Rc::strong_count(&sun)); // 8 references
     uranus.details();
 
     // TODO
-    let neptune = Planet::Neptune(Rc::new(Sun {}));
-    println!("reference count = {}", Rc::strong_count(&sun)); // 9 references
+    let neptune = Planet::Neptune(Rc::clone(&sun));
+    println!("({}) : reference count = {}", line!(), Rc::strong_count(&sun)); // 9 reference
     neptune.details();
 
     assert_eq!(Rc::strong_count(&sun), 9);
@@ -93,12 +91,15 @@ fn main() {
     println!("reference count = {}", Rc::strong_count(&sun)); // 4 references
 
     // TODO
+    drop(earth);
     println!("reference count = {}", Rc::strong_count(&sun)); // 3 references
 
     // TODO
+    drop(venus);
     println!("reference count = {}", Rc::strong_count(&sun)); // 2 references
 
     // TODO
+    drop(mercury);
     println!("reference count = {}", Rc::strong_count(&sun)); // 1 reference
 
     assert_eq!(Rc::strong_count(&sun), 1);
